fa777aa1ec33a0ef9a2f707362d86d8cbe7cfa6f
lucirs7 <luciars787@gmail.com>
2024-05-18 20:29:25 +0200

    Video #56. Added 'following' logic to screen.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,18 +4,26 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function Home({navigation}) {
   const [followRequest, setFollowRequest] = useState<Array<string>>(
     ['John', 'Jane', 'Ram', 'Janice']);
-  const [following, setFollowing] = useState('Hitesh');
+  const [following, setFollowing] = useState<Array<string>>(
+    ['Hitesh']);
 
-  const follow = () => {
+  const follow = (index: number) => {
     const followNew = followRequest.splice(index, 1);
+    following.push(...followNew);
+
+    //setFollowing(following);
   };
 
   return (
     <View style={styles.container}>
-        <Text>MyNavigation!</Text>
+        <Text style={styles.text}>You are following {following.length} friend</Text>
         <Button
           title='Go to follow'
-          onPress={() => navigation.navigate("Follow")}
+          onPress={() => navigation.navigate("Follow", {
+            followRequest_: followRequest,
+            follow_: following,
+            followFunction: follow
+          })}
         />
     </View>
   );
@@ -27,5 +35,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 20,
   },
 });
